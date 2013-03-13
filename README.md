9a6ff401a1c3519263c179ea3e4c6b93be2f49f1
Will Smidlein <will@willsmidlein.com>
2013-03-12 22:17:51 -0400

    It lives!

--- README.md
+++ README.md
@@ -3,10 +3,19 @@ tu-archive-importer
 
 A wonderfully simple tool to import your Twitter archive into ThinkUp.
 
+Instructions
+------------
+Put your tweet JS files in 'tweets'... it should look like this: http://d.pr/i/pkoP
+Run it from the CLI. Config the DB in the actual import.php file (yes, I know, bad practice... but this is a hacky script, so screw you)
+
 Note
 ----
 This is very much beta. Make a backup before you run this.
 
 Thanks
 ------
-Many thanks to [@sarahbourne](https://twitter.com/sarahebourne) for providing her Twitter archive for testing before my account gets the rollout.
+HUGE thanks to [@henriwatson](https://github.com/henriwatson) for writing the tweet parsing code. I procrastinated on it and then he was awesome and did it for me.
+
+Credits
+-------
+[MeekroDB](http://www.meekro.com/) for making me not want to pull my hair out doing MySQL queries.
--- import.php
+++ import.php
@@ -1,13 +1,17 @@
-<pre>
+#!/usr/bin/php
 <?php
 
-date_default_timezone_set('GMT+0');
+// Instead of messing around with SQL statements myself
+require_once 'meekrodb.2.1.class.php';
+DB::$user = 'root';
+DB::$password = 'root';
+DB::$dbName = 'thinkup';
 
 $directory = "tweets/";
 $files = glob($directory . "*.js");
 
-$tweets = Array();
-	
+echo "Running...";
+
 foreach($files as $file)
 {
 
@@ -17,33 +21,30 @@ foreach($files as $file)
 	$data = json_decode($str_data);
 	
 	foreach ($data as $tweet) {
-		$currentTweet = array(
-			'post_id'	 => $tweet->id_str,
-			'author_username'     => $tweet->user->screen_name,
-			'author_fullname'     => $tweet->user->name,
-			'author_avatar'       => $tweet->user->profile_image_url_https,
-			'is_protected'	=> $tweet->user->protected,
-			'author_user_id'      => $tweet->user->id_str,
-			'user_id'	 => $tweet->user->id_str,
-			'post_text'	   => (string)$tweet->text,
-			'pub_date'	=> gmdate("Y-m-d H:i:s", strToTime($tweet->created_at)),
-			'source'	  => (string)$tweet->source,
-			'geo'	   => $tweet->geo,
-			'network'	 => 'twitter'
-		);
+		$parsed_tweet = array(
+	                    'post_id'             => $tweet->id,
+	                    'author_username'     => $tweet->user->screen_name,
+	                    'author_fullname'     => $tweet->user->name,
+	                    'author_avatar'       => $tweet->user->profile_image_url_https,
+	                    'is_protected'        => $tweet->user->protected,
+	                    'author_user_id'      => (string)$tweet->user->id,
+	                    'post_text'           => (string)$tweet->text,
+	                    'pub_date'            => gmdate("Y-m-d H:i:s", strToTime($tweet->created_at)),
+	                    'in_reply_to_post_id' => (string)$tweet->in_reply_to_status_id,
+	                    'in_reply_to_user_id' => (string)$tweet->in_reply_to_user_id,
+	                    'source'              => (string)$tweet->source,
+	                    'place'               => (string)$tweet->place->full_name,
+	                    'network'             => 'twitter'
+	    );
 
-		if (isset($tweet->in_reply_to_post_id) && isset($tweet->in_reply_to_user_id)){
-			$currentTweet['in_reply_to_post_id'] = (string)$tweet->in_reply_to_status_id_str;
-			$currentTweet['in_reply_to_user_id'] = (string)$tweet->in_reply_to_user_id_str;
-		}
+		DB::insert('tu_posts', $parsed_tweet);
 
-		$tweets[] = $currentTweet;
-	}
+    }
+
 }
 
-print_r($tweets);
+$db->close();
 
-
+echo "Finished!";
 
 ?>
-</pre>
