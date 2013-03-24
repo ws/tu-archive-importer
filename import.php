96679a261eca8d142abe53cbd7d419af74358833
Keith Smiley <keithbsmiley@gmail.com>
2013-03-24 17:43:52 -0400

    Update script to check for existing posts with the same ID

--- import.php
+++ import.php
@@ -7,6 +7,9 @@ DB::$user = 'root';
 DB::$password = 'root';
 DB::$dbName = 'thinkup';
 
+// Change this if you changed your posts table prefix
+$table_name = 'tu_posts';
+
 $directory = "tweets/";
 $files = glob($directory . "*.js");
 
@@ -37,10 +40,14 @@ foreach($files as $file)
 	                    'network'             => 'twitter'
 	    );
 
-		DB::insert('tu_posts', $parsed_tweet);
-
+      // Check to see if a tweet with the specific id already exists
+      //  Make sure to only check twitter posts
+      //  Limit the response to 1 (since that should be the maximum there would be
+      $count = DB::queryFirstField("SELECT COUNT(*) FROM %s WHERE post_id=%i AND network=twitter LIMIT 1", $table_name, $parsed_tweet['post_id'])
+      if ($count < 1) {
+        DB::insert($table_name, $parsed_tweet);
+      }
     }
-
 }
 
 $db->close();
