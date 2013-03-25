afb67695f073b4ef8560b1c72017c2e3ad1ffddb
Keith Smiley <keithbsmiley@gmail.com>
2013-03-24 21:41:13 -0400

    Check for non-required keys before adding them

--- import.php
+++ import.php
@@ -25,29 +25,38 @@ foreach($files as $file)
 	
 	foreach ($data as $tweet) {
 		$parsed_tweet = array(
-	                    'post_id'             => $tweet->id,
-	                    'author_username'     => $tweet->user->screen_name,
-	                    'author_fullname'     => $tweet->user->name,
-	                    'author_avatar'       => $tweet->user->profile_image_url_https,
-	                    'is_protected'        => $tweet->user->protected,
-	                    'author_user_id'      => (string)$tweet->user->id,
-	                    'post_text'           => (string)$tweet->text,
-	                    'pub_date'            => gmdate("Y-m-d H:i:s", strToTime($tweet->created_at)),
-	                    'in_reply_to_post_id' => (string)$tweet->in_reply_to_status_id,
-	                    'in_reply_to_user_id' => (string)$tweet->in_reply_to_user_id,
-	                    'source'              => (string)$tweet->source,
-	                    'place'               => (string)$tweet->place->full_name,
-	                    'network'             => 'twitter'
-	    );
+			'post_id'             => $tweet->id,
+			'author_username'     => $tweet->user->screen_name,
+			'author_fullname'     => $tweet->user->name,
+			'author_avatar'       => $tweet->user->profile_image_url_https,
+			'is_protected'        => $tweet->user->protected,
+			'author_user_id'      => (string)$tweet->user->id,
+			'post_text'           => (string)$tweet->text,
+			'pub_date'            => gmdate("Y-m-d H:i:s", strToTime($tweet->created_at)),
+			'source'              => (string)$tweet->source,
+			'network'             => 'twitter'
+		);
 
-      // Check to see if a tweet with the specific id already exists
-      //  Make sure to only check twitter posts
-      //  Limit the response to 1 (since that should be the maximum there would be
-      $count = DB::queryFirstField("SELECT COUNT(*) FROM %s WHERE post_id=%i AND network=twitter LIMIT 1", $table_name, $parsed_tweet['post_id']);
-      if ($count == 0) {
-        DB::insert($table_name, $parsed_tweet);
-      }
-  }
+		if (isset($tweet->place->full_name)) {
+			$parsed_tweet['place'] = (string)$tweet->place->full_name;
+		}
+
+		if (isset($tweet->in_reply_to_status_id)) {
+			$parsed_tweet['in_reply_to_post_id'] = (string)$tweet->in_reply_to_status_id;
+		}
+
+		if (isset($tweet->in_reply_to_user_id)) {
+			$parsed_tweet['in_reply_to_user_id'] = (string)$tweet->in_reply_to_user_id;
+		}
+
+		// Check to see if a tweet with the specific id already exists
+		//  Make sure to only check twitter posts
+		//  Limit the response to 1 (since that should be the maximum there would be
+		$count = DB::queryFirstField("SELECT COUNT(*) FROM " . $table_name . " WHERE post_id=%i AND network='twitter' LIMIT 1", $parsed_tweet['post_id']);
+		if ($count == 0) {
+			DB::insert($table_name, $parsed_tweet);
+		}
+	}
 }
 
 echo "Finished!";
